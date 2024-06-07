326f7da1c9e5ca6ffdc4ce56bfd33f516978e2cb
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2024-06-07 09:56:52 -0300

    created advertising component

---
+++ src/components/publicity/Publicity.jsx
@@ -0,0 +1,9 @@
+import styles from "../publicity/publicity.module.sass"
+
+export default function Publicity() {
+    return(
+        <div className={styles.container}>
+            Publicidade
+        </div>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,15 @@
+import styles from "../styles/index.module.sass"
+
 import Header from "../components/header/Header"
+import Publibcty from "../components/publicity/Publicity"
 
 export default function HomePage() {
     return(
-        <div>
+        <div className={styles.container}>
             <Header />
+            <div className={styles.content}>
+                <Publibcty />
+            </div>
         </div>
     )
 }
