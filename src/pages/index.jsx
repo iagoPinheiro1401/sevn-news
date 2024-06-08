03537f5ecd397429906e155ac69d590aee177141
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2024-06-08 09:59:47 -0300

    request made to show top news data from api using axios

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,28 +6,53 @@ import NewsWithoutImage from "../components/articles/primaries/newsWithouImage/N
 import NewsWithImage from "../components/articles/primaries/newsWithImage/NewsWithImage"
 import SecundaryNews from "../components/articles/secundary/SecundatyNews"
 
+import axios from "axios"
+import { useState, useEffect } from "react"
+
 export default function HomePage() {
+    const [primaryNews, setPrimaryNews] = useState('')
+
+    useEffect(() => {
+        const fetchNewsData = async () => {
+            try {
+                const response = await axios.get('https://sevn-news-api-mu.vercel.app/api/artigos-principais')
+                setPrimaryNews(response.data)
+            } catch (error) {
+                console.error('Error fetching news data:', error)
+            }
+            }
+        
+            fetchNewsData();
+    }, [])
+
     return(
         <div className={styles.container}>
             <Header />
             <div className={styles.content}>
                 <Publibcity />
                 <section className={styles.primaryNewsContainer}>
-                    <NewsWithoutImage 
-                        category="economia" 
-                        title="Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases, diz BC"
-                    />
+                    {primaryNews[0] &&
+                        <NewsWithoutImage 
+                            category={primaryNews[0].categoria}
+                            title="Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases, diz BC"
+                        />
+                    }
                     <div className={styles.newsWithImageContainer}>
-                        <NewsWithImage
-                            category="educação" 
-                            title="Datafolha: Após ensino remoto, 76% precisam de reforço na alfabetização" 
-                            img="aula-pandemia.png"
-                        />
-                        <NewsWithImage 
-                            category="diversidades" 
-                            title="Lotomania: com prêmio de R$ 5 milhões, veja os números sorteados hoje" 
-                            img="cedulas.png"
-                        />
+                        {primaryNews[1] &&
+                            <NewsWithImage
+                                    category={primaryNews[1].categoria} 
+                                    title="Datafolha: Após ensino remoto, 76% precisam de reforço na alfabetização" 
+                                    img="aula-pandemia.png"
+                            />
+                        }
+
+                        {primaryNews[2] && 
+                            <NewsWithImage 
+                                category={primaryNews[2].categoria} 
+                                title="Lotomania: com prêmio de R$ 5 milhões, veja os números sorteados hoje" 
+                                img="cedulas.png"
+                            />
+                        }
                     </div>
                 </section>
                 <div className={styles.secundaryNewsContainer}>
