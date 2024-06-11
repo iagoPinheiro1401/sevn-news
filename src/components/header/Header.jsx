c60b34e89552d384899bef5c44d456ccdc81282a
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2024-06-11 10:11:48 -0300

    created function in the header to return to homePage

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,13 +2,20 @@ import styles from "../header/header.module.sass"
 
 import { ArrowLeft } from "@phosphor-icons/react"
 import PropTypes from "prop-types"
+import { useRouter } from "next/router"
 
 export default function Header({ homePageBack }) {
+    const router = useRouter()
+
+    const handleClick = () => {
+        router.push("/")
+    }
+
     return(
         <header className={styles.header}>
             {homePageBack &&
                 <button>
-                    <ArrowLeft size={24} color="#fafafa" />
+                    <ArrowLeft size={24} color="#fafafa" onClick={handleClick}/>
                 </button>
             }
             <div className={styles.container}>
